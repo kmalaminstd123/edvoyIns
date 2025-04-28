4dab10ff48af35f05b1284f7594182aad669c62a
K.M. AL-AMIN <alaminkhanstd123@gmail.com>
2025-04-28 18:35:34 +0600

    search bar appear on onscroll

--- js/main.js
+++ js/main.js
@@ -1,13 +1,18 @@
 
 // navbar js
 const navbar = document.querySelector('.nav-desn')
+const navbarForm = document.querySelector('.nav-form')
 
 window.addEventListener('scroll', e=>{
     
     if(window.scrollY >= 500){
         navbar.classList.add('navbar-scroll')
+        navbarForm.style.display = 'block'
+        navbarForm.style.transition = 'all 0.5s linear'
     }else{
         navbar.classList.remove('navbar-scroll')
+        navbarForm.style.display = 'none'
+        navbarForm.style.transition = 'all 0.5s linear'
     }
     
 })
